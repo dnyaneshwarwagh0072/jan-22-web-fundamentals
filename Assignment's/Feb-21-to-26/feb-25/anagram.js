5c7cb377eaa6aba4f680ceca8b5fc9233e219bf2
Dnyaneshwar Yadav Wagh <85550894+nanawagh@users.noreply.github.com>
2022-03-04 23:11:38 +0530

    Add files via upload

---
+++ Assignment's/Feb-21-to-26/feb-25/anagram.js
@@ -0,0 +1,26 @@
+let input1 = prompt('Enter the first String');
+let input2 = prompt('Enter the Second String');
+var counter = 0;
+function anagram(tosort1,tosort2) {
+  let split1 = tosort1.split("");
+  let split2 = tosort2.split("");
+  let str1 = split1.sort();
+  let str2 = split2.sort();
+  let len1 = str1.length;
+  let len2 = str2.length;  
+  for(let i = 0; i < len1; i++){
+    if(len1 == len2) {
+        if(str1[i] == str2[i]){
+            counter++;
+        }
+    }
+    break;
+  }
+}
+anagram(input1,input2);
+if(counter == 0){
+    document.write(`${input1} and ${input2} is not Anagram`);
+}
+else {
+    document.write(`${input1} and ${input2} is Anagram`);
+}

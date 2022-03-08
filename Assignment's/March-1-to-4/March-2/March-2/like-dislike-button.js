10c407d80f2679bf19ca592f3da6ae232f6652e2
Dnyaneshwar Yadav Wagh <85550894+nanawagh@users.noreply.github.com>
2022-03-08 23:10:31 +0530

    Add files via upload

---
+++ Assignment's/March-1-to-4/March-2/March-2/like-dislike-button.js
@@ -0,0 +1,9 @@
+var counter = [0,0,0,0,0,0,0,0]
+function like(n) {
+    counter[n] += 1;
+    document.getElementById(`disp${n}`).innerText=counter[n];
+}
+function dislike(n) {
+    counter[n] -= 1;
+    document.getElementById('disp').innerText=counter;
+}
